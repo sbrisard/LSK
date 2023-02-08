f73b113eed4335fb2e33cfef0a37fb90820911cb
Sébastien Brisard <sebastien.brisard@m4x.org>
2023-02-08 06:52:42 +0100

    New python module `lsk.py`.

---
+++ quarto/lsk.py
@@ -0,0 +1,26 @@
+import IPython.display
+import pygments
+import sympy
+
+
+def latex(x):
+    return x if str(x) == x else sympy.latex(x)
+
+
+def display_latex_str(s):
+    display(IPython.display.Math(s))
+
+
+def display_latex_dict(d, num_cols=2):
+    s = "\\begin{align}"
+    col = 1
+    for k, v in d.items():
+        s += latex(k) + "&=" + latex(v)
+        s += r"\\" if col % num_cols == 0 else r"&"
+        col += 1
+    s += "\\end{align}"
+    display_latex_str(s)
+
+
+def display_latex_equation(lhs, rhs):
+    display_latex_str(latex(lhs) + "=" + latex(rhs))

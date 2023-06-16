3bf4208f40b1e0728f542574fd2c467921d27bc4
Sébastien Brisard <sbrisard@users.noreply.github.com>
2023-06-16 21:17:32 +0200

    Factor out simplification rules

--- quarto/lsk.py
+++ quarto/lsk.py
@@ -2,7 +2,7 @@ import IPython.display
 import json
 import sympy
 
-from sympy import Add, expand, Integer, Mul, Pow, Rational, Symbol
+from sympy import Add, expand, Idx, IndexedBase, Integer, Mul, Pow, Rational, Symbol
 
 
 def __latex(x):
@@ -41,6 +41,93 @@ u0_dot = Symbol(r"\dot{u}_0")
 u0_ddot = Symbol(r"\ddot{u}_0")
 u0_dddot = Symbol(r"\dddot{u}_0")
 u0_ddddot = Symbol(r"\ddddot{u}_0")
+
+# Standard indices, to be used in finalized expressions
+i = Idx("i")
+j = Idx("j")
+k = Idx("k")
+l = Idx("l")
+
+# Instances of the `IndexedBase` class end with `_`
+
+λ_ = IndexedBase(r"\lambda")
+ξ_ = IndexedBase(r"\xi")
+v_ = IndexedBase("v")
+w_ = IndexedBase("w")
+E_ = IndexedBase("E")
+E_dot_ = IndexedBase("\dot{E}")
+E_ddot_ = IndexedBase("\ddot{E}")
+
+rules = dict()
+_λ = Idx(r"\lambda")
+
+rules["E_ij"] = {
+    E2 * v_[i]: 0,
+    E2 * v_[j]: 0,
+    E2 * v_[k]: 0,
+    E2 * v_[l]: 0,
+}
+
+rules[r"\dot{E}_ij"] = {
+    E2_dot * v_[i] * v_[j]: E_dot_[i, j],
+    E2_dot * v_[i] * v_[k]: E_dot_[i, k],
+    E2_dot * v_[i] * v_[l]: E_dot_[i, l],
+    E2_dot * v_[j] * v_[k]: E_dot_[j, k],
+    E2_dot * v_[j] * v_[l]: E_dot_[j, l],
+    E2_dot * v_[k] * v_[l]: E_dot_[k, l],
+}
+
+rules[r"\ddot{E}_ij"] = {
+    E2_ddot * v_[i] * v_[j]: (E_ddot_[i, j]
+                              - E2_dot * v_[i] * w_[j, _λ]
+                              - E2_dot * v_[j] * w_[i, _λ]),
+    E2_ddot * v_[i] * v_[k]: (E_ddot_[i, k]
+                              - E2_dot * v_[i] * w_[k, _λ]
+                              - E2_dot * v_[k] * w_[i, _λ]),
+    E2_ddot * v_[i] * v_[l]: (E_ddot_[i, l]
+                              - E2_dot * v_[i] * w_[l, _λ]
+                              - E2_dot * v_[l] * w_[i, _λ]),
+}
+
+rules[r"E_ijk"] = {
+    E3 * v_[i] * v_[j] * v_[k]: E_[i, j, k],
+    E3 * v_[i] * v_[j] * v_[l]: E_[i, j, l],
+    E3 * v_[i] * v_[k] * v_[l]: E_[i, k, l],
+}
+
+rules[r"\dot{E}_ijk"] = {
+    E3_dot * v_[i] * v_[j] * v_[k]: (E_dot_[i, j, k] - E2_dot * v_[i] * w_[j, k]
+                                     - E2_dot * v_[j] * w_[i, k] - E2_dot * v_[k] * w_[i, j]),
+    E3_dot * v_[i] * v_[j] * v_[l]: (E_dot_[i, j, l] - E2_dot * v_[i] * w_[j, l]
+                                     - E2_dot * v_[j] * w_[i, l] - E2_dot * v_[l] * w_[i, j]),
+    E3_dot * v_[i] * v_[k] * v_[l]: (E_dot_[i, k, l] - E2_dot * v_[i] * w_[k, l]
+                                     - E2_dot * v_[k] * w_[i, l] - E2_dot * v_[l] * w_[i, k]),
+}
+
+rules["E_ijkl"] = {
+    E4 * v_[i] * v_[j] * v_[k] * v_[l]: (E_[i, j, k, l]
+                                         - E3 * v_[i] * v_[j] * w_[k, l]
+                                         - E3 * v_[i] * v_[k] * w_[j, l]
+                                         - E3 * v_[i] * v_[l] * w_[j, k])
+}
+
+rules["w_ij = w_ji"] = {
+    w_[j, i]: w_[i, j],
+    w_[k, i]: w_[i, k],
+    w_[k, j]: w_[j, k],
+    w_[l, i]: w_[i, l],
+    w_[l, j]: w_[j, l],
+    w_[l, k]: w_[k, l]
+}
+
+rules["λ_ij = λ_ji"] = {
+    λ_[j, i]: λ_[i, j],
+    λ_[k, i]: λ_[i, k],
+    λ_[k, j]: λ_[j, k],
+    λ_[l, i]: λ_[i, l],
+    λ_[l, j]: λ_[j, l],
+    λ_[l, k]: λ_[k, l]
+}
 
 E_uλ = -E2 * u0_dot
 E_uλλ = -E2 * u0_ddot - 2 * E2_dot * u0_dot + E3 * u0_dot**2
@@ -102,19 +189,19 @@ def create_u_star(λ_fun=default_λ_fun):
             + λ_fun() * λ_fun() * λ_fun() * u0_dddot / 6
             + λ_fun() * λ_fun() * λ_fun() * λ_fun() * u0_ddddot / 24)
 
+def autotest():
+    with open("setting-up_the_computational_stage.out.json", "r") as f:
+        data = json.load(f)
 
-with open("setting-up_the_computational_stage.out.json", "r") as f:
-    data = json.load(f)
+    assert E_uλ == eval(data["E_uλ"])
+    assert E_uλλ == eval(data["E_uλλ"])
+    assert E_uλλλ == eval(data["E_uλλλ"])
+    assert E_uuλ == eval(data["E_uuλ"])
+    assert E_uuλλ == eval(data["E_uuλλ"])
+    assert E_uuuλ == eval(data["E_uuuλ"])
 
-assert E_uλ == eval(data["E_uλ"])
-assert E_uλλ == eval(data["E_uλλ"])
-assert E_uλλλ == eval(data["E_uλλλ"])
-assert E_uuλ == eval(data["E_uuλ"])
-assert E_uuλλ == eval(data["E_uuλλ"])
-assert E_uuuλ == eval(data["E_uuuλ"])
+    assert expand(create_E() - eval(data["E"])) == 0
+    assert expand(create_E_u() - eval(data["E_u"])) == 0
+    assert expand(create_E_uu() - eval(data["E_uu"])) == 0
 
-assert expand(create_E() - eval(data["E"])) == 0
-assert expand(create_E_u() - eval(data["E_u"])) == 0
-assert expand(create_E_uu() - eval(data["E_uu"])) == 0
-
-assert expand(create_u_star() - eval(data["u_star"])) == 0
+    assert expand(create_u_star() - eval(data["u_star"])) == 0
